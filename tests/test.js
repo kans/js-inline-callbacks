f09f2a8f314b213055ba293c3c449ace8f60cef2
Matt Kaniaris <mkaniaris@gmail.com>
2015-02-20 16:37:05 -0800

    some working tests

---
+++ tests/test.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var inline = require("../lib/inline_callbacks");
+
+exports.testBasic = function(test){
+  function* a () {
+    yield 1;
+    yield 2;
+    test.ok(true, "this assertion should pass");
+    test.done();
+  };
+
+  test.expect(1);
+  inline(a)
+};
+
+exports.testReturnByReference = function(test){
+  function* a (ret) {
+    let yeilded = 1;
+    yield yeilded;
+    test.equals(ret[0], yeilded, "this assertion should pass");
+    yeilded = 2;
+    yield function (cb) {
+      setTimeout(function () {return cb(yeilded)}, 0);
+    }
+    test.equals(ret[0], yeilded, "this assertion should pass");
+    test.done();
+  };
+
+  test.expect(2);
+  inline(a)
+};
